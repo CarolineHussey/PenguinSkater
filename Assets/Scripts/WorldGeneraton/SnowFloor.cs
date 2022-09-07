d854e97a47dcd8e3c9d27307f5b02054febca775
CarolineHussey <66087850+CarolineHussey@users.noreply.github.com>
2022-09-07 16:28:27 +0100

    Bug Fixes

---
+++ Assets/Scripts/WorldGeneraton/SnowFloor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowFloor : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Material material;
+    public float offsetSpeed = 0.25f;
+
+    private void Update()
+    {
+        transform.position = Vector3.forward * player.transform.position.z;
+        material.SetVector("snowOffset", new Vector2(0, -transform.position.z * offsetSpeed));
+    }
+}
